3ece8488f8bc38319bb384a2ddfae48d44339fa0
Aidan Grimshaw <grimshawaidan@gmail.com>
2019-02-17 14:48:45 -0800

    got test function working

--- KarelSemantics.hs
+++ KarelSemantics.hs
@@ -6,10 +6,47 @@ import Data.Function (fix)
 import KarelSyntax
 import KarelState
 
+-- type Robot = (Pos,Card,Int)
+pos1 :: Pos
+pos1 = (1,1)
+
+rob1 :: Robot
+rob1 = (pos1,South,0)
+
+-- type World = Pos -> Maybe Int
+w1 :: World
+w1 _ = Just 2
+
+-- testing facing direction working
+-- test (Facing South) w1 rob1
+-- True
+
+-- testing negation working
+-- test (Not (Facing South)) w1 rob1
+-- False
+
+-- Testing empty working
+-- test Empty w1 rob1
+-- True
+
+-- Testing beeper working
+-- test Beeper w1 rob1
+-- True
+
+-- Testing clear working
+-- test (Clear Left) (w1) rob1
+-- True
+
 
 -- | Valuation function for Test.
 test :: Test -> World -> Robot -> Bool
-test = undefined
+test (Facing card) _ robot = (card == (getFacing robot))
+test (Not testT) world robot = not (test testT world robot)
+test Beeper world robot = hasBeeper (getPos robot) world
+test Empty _ robot = isEmpty robot 
+test (Clear dir) world robot = isClear (relativePos dir robot) world
+
+-- (cardTurn dir (getFacing robot))
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
