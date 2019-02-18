dc9e6cd5c77060b5262344b62f6570ce689ddd80
Aidan Grimshaw <grimshawaidan@gmail.com>
2019-02-17 23:31:13 -0800

    got putBeeper function working

--- KarelSemantics.hs
+++ KarelSemantics.hs
@@ -13,6 +13,14 @@ pos1 = (1,1)
 rob1 :: Robot
 rob1 = (pos1,South,0)
 
+rob2 :: Robot
+rob2 = (pos1,South,1)
+
+
+-- type Defs = [(Macro,Stmt)]
+def1 :: Defs
+def1 = [("test",Shutdown)]
+
 -- type World = Pos -> Maybe Int
 w1 :: World
 w1 _ = Just 2
@@ -48,6 +56,13 @@ test (Clear dir) world robot = isClear (relativePos dir robot) world
 
 -- (cardTurn dir (getFacing robot))
 
+-- Testing PutBeeper
+-- stmt PutBeeper def1 w1 rob2
+-- OK: ((1,1),South,1)
+
+-- stmt PutBeeper def1 w1 rob1
+-- Error: Beeper bag empty
+
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
 stmt Shutdown   _ _ r = Done r
@@ -55,6 +70,11 @@ stmt PickBeeper _ w r = let p = getPos r
                         in if hasBeeper p w
                               then OK (decBeeper p w) (incBag r)
                               else Error ("No beeper to pick at: " ++ show p)
+stmt PutBeeper _ world robot = 
+                        if  not (isEmpty robot)
+                            then OK (incBeeper (getPos robot) world) (decBag robot)
+                            else Error ("Beeper bag empty")
+-- stmt (Turn dir) _ world robot = cardTurn dir 
 stmt _ _ _ _ = undefined
     
 -- | Run a Karel program.
